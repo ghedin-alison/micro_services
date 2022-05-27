57e4f60afdf67c5ed8468a913b84946316feb441
Alison Ghedin <alisonjghedin@gmail.com>
2022-05-27 16:57:46 -0300

    preparing serializers and views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list_products(self, request):  # get /api/products
+        pass
+
+    def create_product(self, request):  # post /api/products
+        pass
+
+    def retrieve_product(self, request, pk=None):  # get /api/products/<str:id>
+        pass
+
+    def update_product(self, request, pk=None):  # update /api/products/<str:id>
+        pass
+
+    def destroy_product(self, request, pk=None):  # delete /api/products/<str:id>
+        pass
