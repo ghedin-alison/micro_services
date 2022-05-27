f9b6f2f1114e24137e93a377b62aa76163531fc2
Alison Ghedin <alisonjghedin@gmail.com>
2022-05-27 17:09:29 -0300

    creating product urls

---
+++ products/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from views import ProductViewSet
+
+urlpatterns = [
+    path('products', ProductViewSet.as_view({
+        'get': 'list_products',
+        'post': 'create_product'
+    })),
+    path('products/<str:pk>', ProductViewSet.as_view({
+        'get': 'retrieve_product',
+        'update': 'update_product',
+        'delete': 'destroy_product',
+})),
+]
+
