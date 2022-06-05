4e273aac43c683ddbf74dd062889de9d1ece9505
Alison Ghedin <alisonjghedin@gmail.com>
2022-06-05 20:42:12 -0300

    finished as is as the tutorial

--- consumer.py
+++ consumer.py
@@ -1,4 +1,12 @@
+import django
+import json
+import os
 import pika
+
+from products.models import Product
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin.settings')
+django.setup()
 
 params = pika.URLParameters('amqps://liltmcjs:uuslkpHY75_na5OufuekpazLaon1uR8H@rat.rmq2.cloudamqp.com/liltmcjs')
 
@@ -11,7 +19,12 @@ channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
     print('Received in admin')
-    print(body)
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes += 1
+    product.save()
+    print('Product likes increased!')
 
 
 channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
